8f0cc6d14fe41e979e31fc1720068bc161657019
Adam Wolszleger <wolszlegeradam@gmail.com>
2022-11-14 21:55:21 +0100

    Dodanie generatora klucza

---
+++ KeyGenerator/KeyGenerator/Program.cs
@@ -0,0 +1,9 @@
+﻿using System.Security.Cryptography;
+
+var randomNumber = new byte[16];
+
+using (var rng = RandomNumberGenerator.Create())
+{
+    rng.GetBytes(randomNumber);
+    Console.WriteLine(Convert.ToBase64String(randomNumber));
+}
